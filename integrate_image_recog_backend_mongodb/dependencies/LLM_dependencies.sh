007000dfc0544e2e61c6e9c8aa7650464e1a31e5
Lu Li <greenpearll0804@gmail.com>
2024-12-03 00:27:19 -0800

    dependencies of all env

---
+++ integrate_image_recog_backend_mongodb/dependencies/LLM_dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#install transformers
+pip install transformers
+
+#install pytorch with GPU support version
+pip install torch torchvision torchaudio
+
+# install required dependencies einops
+pip install einops
+
+#install flask
+pip install flask
